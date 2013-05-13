927135ce47b4cbadda66646ebe495d4cd5447104
Frank Houthuijzen <frank_h@hotmail.nl>
2013-05-13 09:38:32 +0200

    php
    lol

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+<?php
+
+function codeform() {
+echo '<form>
+ Voeg hier je code in.<input type="text" name="inlogcode"> 
+ <input type="Submit" value="Verzenden">
+ </form>'
+}
+
+?>
+
+
+</head>
+
+<body>
+<?php
+$code = "testcode123";
+
+
+if (!isset($_POST['code'])) 
+{
+codeform();
+exit;
+
+}
+
+?>
+
+
+</body>
+
+</html>
