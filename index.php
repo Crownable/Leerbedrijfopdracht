44d31f8ce98df76449e55a605220fa8f5452920c
Danny Hillebrand <danny-hillebrand@live.nl>
2013-05-13 09:51:03 +0200

    ....
    .....

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <html>
 
 <head>
+<<<<<<< HEAD
 <link Href='style.css' type='text/css' rel='stylesheet' >
+=======
+>>>>>>> 2927655f8ea2e7658b53bcd80fc455c4fe25ad39
 <?php
 
 function codeform() {
 echo '<form>
+<<<<<<< HEAD
  Voeg hier je code in.<input type="text" name="inlogcode"> 
  <input type="Submit" value="Verzenden">
  </form>'
+=======
+ Voeg hier je code in:<input type="text" name="inlogcode"> 
+ <input type="Submit" value="Verzenden">
+ </form>';
+>>>>>>> 2927655f8ea2e7658b53bcd80fc455c4fe25ad39
 }
 
 ?>
@@ -19,6 +28,7 @@ echo '<form>
 <body>
 <?php
 $code = "testcode123";
+<<<<<<< HEAD
 
 
 if (!isset($_POST['code'])) 
@@ -33,6 +43,19 @@ exit;
 	Voeg hier je code in.<input type='text' name='inlogcode'> 
 	<input type='Submit' value='Verzenden'>
 	</form>
+=======
+
+
+if (!isset($_POST['code'])) 
+{
+codeform();
+exit;
+
+}
+
+?>
+
+>>>>>>> 2927655f8ea2e7658b53bcd80fc455c4fe25ad39
 
 ?>
 
