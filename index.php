1fda55fd38a43674854864400a0ecd9e7b81856a
Danny Hillebrand <danny-hillebrand@live.nl>
2013-05-13 09:50:21 +0200

    TEST
    .....

--- index.php
+++ index.php
@@ -1,14 +1,41 @@
 <html>
 
 <head>
+<link Href='style.css' type='text/css' rel='stylesheet' >
+<?php
+
+function codeform() {
+echo '<form>
+ Voeg hier je code in.<input type="text" name="inlogcode"> 
+ <input type="Submit" value="Verzenden">
+ </form>'
+}
+
+?>
+
+
 </head>
 
 <body>
+<?php
+$code = "testcode123";
+
+
+if (!isset($_POST['code'])) 
+{
+codeform();
+exit;
+
+}
+
 
 	<form>
 	Voeg hier je code in.<input type='text' name='inlogcode'> 
 	<input type='Submit' value='Verzenden'>
 	</form>
+
+?>
+
 
 </body>
 
